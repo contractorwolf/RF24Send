1837d58bccc9d7dbf3ef1228e2918109a2383a36
contractorwolf <jameswolf@jamess-MacBook-Pro.local>
2014-09-15 00:17:14 -0400

    added success counts and continuous stream of data

--- RF24Send.ino
+++ RF24Send.ino
@@ -8,7 +8,12 @@ RF24 radio(9,10);
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+int timeout_period = 200;
 
+long success_count = 0;
+long failure_count = 0;
+long total_count = 0;
+float success_rate = 0;
 
 void setup(void)
 {
@@ -20,10 +25,8 @@ void setup(void)
 
   // Setup and configure rf radio
   radio.begin();
-
   // optionally, increase the delay between retries & # of retries
   radio.setRetries(15,15);
-
   // optionally, reduce the payload size.  seems to improve reliability
   // radio.setPayloadSize(8);
   
@@ -34,49 +37,82 @@ void setup(void)
   radio.startListening();
 
   // Dump the configuration of the rf unit for debugging
-  radio.printDetails();
+  radio.printDetails();//doesnt work without printf
 }
 
 void loop(void)
 {
+
+  //OPTION 1: continous stream of random values
+  boolean success = SendMessage(random(99999999));  
+  if(success){
+    Serial.println("SEND Successful");
+    success_count++;
+  }else{
+    Serial.println("SEND FAILED");
+    failure_count++;
+  }
+
+  total_count = success_count + failure_count;
+  success_rate = ((float)success_count*100/(float)total_count); 
+  
+  Serial.print("Total: ");
+  Serial.print(total_count);
+  Serial.print(" Success: ");
+  Serial.print(success_count);
+  Serial.print(" Failure: ");
+  Serial.print(failure_count);
+  Serial.print(" Success Rate: ");
+  Serial.print(success_rate);
+  Serial.println("%");  
+  
+  /*
+  //OPTION 2: wait for a number to come through serial stream
   if ( Serial.available() )
   {
     boolean success = SendMessage(Serial.parseFloat());
-      
+
     if(success){
       Serial.println("SEND Successful");
+      success_count++;
     }else{
       Serial.println("SEND FAILED");
+      failure_count++;
     }
   }
+  */
+  
+  //delay(10);//doesnt seem to be needed
 }
 
 // sends a message that can be as large as a long
 boolean SendMessage(unsigned long message){
     long start = millis();
-  
     boolean success = false;
-    radio.stopListening();
-
+    
     Serial.print("Now sending...");
-    Serial.print(message);
+    Serial.print(message);    
+    
+    //pause listening to send
+    radio.stopListening();
     
     // Take the message and send it.  This will block until complete
     bool ok = radio.write( &message, sizeof(unsigned long) );
+
+    // Now, continue listening
+    radio.startListening();
     
     if (ok)
       Serial.println(" ok...");
     else
-      Serial.println(" FAILED.");
-
-    // Now, continue listening
-    radio.startListening();
+      Serial.println(" FAILED.");    
+    
 
     // Wait here until we get a response, or timeout (250ms)
     unsigned long started_waiting_at = millis();
     bool timeout = false;
     while ( ! radio.available() && ! timeout )
-      if (millis() - started_waiting_at > 200 )
+      if (millis() - started_waiting_at > timeout_period )
         timeout = true;
 
     // Describe the results
@@ -86,7 +122,7 @@ boolean SendMessage(unsigned long message){
     }
     else
     {
-      // Grab the response, compare, and send to debugging spew
+      // Grab the response, comparen it to what was sent
       unsigned long got_message;
       radio.read( &got_message, sizeof(unsigned long) );
 
@@ -103,5 +139,7 @@ boolean SendMessage(unsigned long message){
     
     Serial.print("roundtrip time: ");
     Serial.println(millis() - start);
+    
+    return(success);
 }
 
